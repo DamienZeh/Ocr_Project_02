afa5b85b3ac54c01e1717f36e36162fcf8702277
damie <damien.hernandez1@hotmail.fr>
2021-10-13 18:58:43 +0200

    Ajout fonction écriture images + dossier de sortie

--- Analyze_Products_From_Category.py
+++ Analyze_Products_From_Category.py
@@ -1,6 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+import os
+
+
+def main(url_category):
+    creation_csv()
+    path = 'Images_Book_Analyze_Products_From_Category' # répertoire de sortie pour les images.
+    try:                                    # On teste si le dossier existe déjà.
+        os.mkdir('Images_Book_Analyze_Products_From_Category')
+    except:
+        print('directory already exists')
+
+    for pages in etl_pages_category("https://books.toscrape.com/catalogue/category/books/nonfiction_13/index.html"):
+        for books in etl_books_in_page(pages):
+            writer_data_book_csv(etl_book(books))
+
+            data_book = etl_book(books)
+            title_book = data_book[2]  # on récupère le nom du livre et le lien de l'image_url pour enregistrer l'image.
+            image_url = data_book[-1]
+            writer_image_book(title_book, image_url, path)
+
+
+
+
 
 
 def etl_pages_category(url_pages_category): # récupère toutes les pages d'une catégorie
@@ -25,11 +48,7 @@ def etl_pages_category(url_pages_category): # récupère toutes les pages d'une 
         pages_category.append(url_category_index)                #on stocke toutes les pages de la categorie dans la liste 'pageCategory'.
 
 
-    creation_csv()         #On crée le csv avec l'entête.
-
-
-    for page in pages_category:    #on boucle sur la fonction etl_books_in_page()
-        etl_books_in_page(page)
+    return pages_category
 
 
 def etl_books_in_page(url_page_livres):             # récupère tous les livres d'une page
@@ -45,29 +64,22 @@ def etl_books_in_page(url_page_livres):             # récupère tous les livres
     replace_link_books= str_list_books.replace("../../../", debut_de_lien) #on remplace le lien manquant
     books = replace_link_books.split(',')               # on a notre liste de liens pour les livres de la page
 
-    for book in books:     #on boucle sur la fonction etl_book()
-        etl_book(book)
+    return books
 
 
 def etl_book(url_book): #on récupère les détails du livre
     page = requests.get(url_book)
     soup = BeautifulSoup(page.content, 'html.parser')
     details_prod_upc_tax_available = soup.find_all('td')  # ici on trouve l'UPC, les taxes et la disponibilité
+    universal_product_code = ''.join(details_prod_upc_tax_available[0])     # code UPC
+    title_name = ''.join(soup.find('li', class_="active"))    # titre.
+    title = title_name[:90].replace(',', '_').replace("'", '_').replace(':', '_').replace('/', '_').replace('"', '_').replace('*', '_')\
+        .replace('?', '.').replace('#', '_').replace('%', '_').replace('-', '_').replace('é', 'e').replace('è', 'e')\
+        .replace('à', 'a').replace('â', 'a').replace('â', 'a').replace(' ', '_')#sans caractères spéciaux, limite taille
 
-    # code UPC
-    universal_product_code = ''.join(details_prod_upc_tax_available[0])
-
-    # titre
-    titles = ''.join(soup.find('li', class_="active"))
-
-    # price_including_tax
-    price_including_tax = ''.join(details_prod_upc_tax_available[3])
-
-    # price_excluding_tax
-    price_excluding_tax = ''.join(details_prod_upc_tax_available[2])
-
-    # available
-    number_available = ''.join(details_prod_upc_tax_available[5])
+    price_including_tax = ''.join(details_prod_upc_tax_available[3])         # price_including_tax
+    price_excluding_tax = ''.join(details_prod_upc_tax_available[2])           # price_excluding_tax
+    number_available = ''.join(details_prod_upc_tax_available[5])         # available
 
     # description
     try:                       #il y a des livres qui n'ont pas de description, on gère cette possible erreur ici, avec un try except.
@@ -75,6 +87,7 @@ def etl_book(url_book): #on récupère les détails du livre
         product_description = ''.join(product_descriptions[3])  # 3 est la position de la description en 'p' dans la class product_page
     except:
         product_description = "pas de description pour ce livre."
+
 
     # category
     categories_all = soup.find('ul', class_="breadcrumb").find_all('a')
@@ -104,30 +117,35 @@ def etl_book(url_book): #on récupère les détails du livre
     str_img_url = "".join(img_url)
     image_url = str_img_url.replace("../../", "https://books.toscrape.com/")  # on a notre lien image url.
 
-    data_book = url_book, universal_product_code, titles, price_including_tax, price_excluding_tax, number_available, product_description, categories, reviews_rating, image_url
-    writer_data_book_csv(data_book)
+    # data_book récupère toute les infos du livre
+    data_book = url_book, universal_product_code, title, price_including_tax, price_excluding_tax, number_available, product_description, categories, reviews_rating, image_url
+    return data_book
+
+def writer_image_book(title, image_url, path):
+    response = requests.get(image_url)
+    with open(path+f'/{title}.jpg', 'wb') as image_file:
+        image_file.write(response.content)
+
 
 # crée le csv avec l'entête.
 def creation_csv():
     #en tête de la fiche
     heading = ["product_page_url", "universal_product_code (upc)", "title","price_including_tax", "price_excluding_tax", "number_available", "product_description", "category","review_rating", "image_url" ]
-    with open('Analyze_Products_From_Category.csv', 'w', encoding='utf-8', errors='ignore') as fichier_csv:
+    with open('Analyze_Products_From_Category.csv', 'w', encoding='utf-8', errors='ignore') as fichier_csv:     # 'encoding='utf-8', errors='ignore', permet d'éviter les UnicodeError
             writer = csv.writer(fichier_csv, delimiter=',')
             writer.writerow(heading)
 
 # écrit les données de tous les livres de toutes les pages d'une catégorie.
 def writer_data_book_csv(data_book):
-    with open('Analyze_Products_From_Category.csv', 'a', encoding='utf-8', errors='ignore') as fichier_csv:
+    with open('Analyze_Products_From_Category.csv', 'a', encoding='utf-8', errors='ignore') as fichier_csv:   # 'encoding='utf-8', errors='ignore', permet d'éviter les UnicodeError
         writer = csv.writer(fichier_csv, delimiter=',')
         writer.writerow(data_book)
 
 
 
 #tapez dans les parenthèses, avec les guillemets, le lien de la première page de la catégorie désirée.
-etl_pages_category("https://books.toscrape.com/catalogue/category/books/nonfiction_13/index.html")
+main("https://books.toscrape.com/catalogue/category/books/nonfiction_13/index.html")
 
 
 
 
-#NOTE:
-# dans 'with open', mettre 'encoding='utf-8', errors='ignore', permet d'éviter les UnicodeError
